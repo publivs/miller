221773e595c41a40488465de69ca898298bfd0d3
miller <517051861@qq.com>
2019-07-16 17:45:19 +0800

    DayOne

---
+++ info/constants.py
@@ -0,0 +1,28 @@
+IMGAE_CODE_REDIS_EXPIRES = 300
+#图片验证码redis有效期,单位:s
+SMS_CODE_REDDIS_EXPIRES = 300
+#　短信验证码redis有效期
+QINIU_DOMIN_PREFIX = "http://oyucyko3w.bkt.clouddn.com/"
+#　七牛空间域名
+HOME_PAGE_MAX_NEWS = 10
+#　首页展示最多的新闻数量
+
+USER_FOLLOWED_MAX_COUNT = 4
+#　用户关注每一页最多的数量
+
+USER_COLLECTION_MAX_NEWS = 10
+#用户收藏最多新闻数量
+
+OTHER_COLLECTION_MAX_NEWS_COUNT = 10
+#其他用户每一页最多新闻数量
+
+CLICK_RANK_MAX_NEWS = 10
+#点击排行展示最多的的新闻数据
+
+ADMIN_USER_PAGE_MAX_NEWS=10
+#管理员页面用户煤业最多数据调试
+
+ADMIN_NEWS_PAGE_MAX_COUNT= 10
+#管理员页面新闻每页最多的数据条数
+
+
---
+++ info/modules/index/views.py
@@ -0,0 +1,9 @@
+from . import index_blu
+
+
+@index_blu.route('/')
+def index():
+    return '<h1>index-text</h1>'
+
+
+
