77b733709637bb279c341f61fbc209226c1d680c
chenqian <517051861@qq.com>
2022-12-16 23:36:22 +0800

    update_votes

--- test_cath_data/auto_vote.py
+++ test_cath_data/auto_vote.py
@@ -9,6 +9,8 @@ import pytesseract
 from PIL import Image
 import cv2 as cv
 import numpy as np
+import random
+
 
 chrome_options = Options()
 chrome_options.add_argument('window-size=1920x3000') #指定浏览器分辨率
@@ -22,6 +24,43 @@ chrome_options.add_argument('--disable-blink-features=AutomationControlled')  # 
 chrome_options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36')
 
 # chrome_options.add_argument('--headless') # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败,可视化带ui的正常使用,方便调试
+
+# # 代理池处理
+# proxy_arr = pool_list = [
+#     "111.225.152.36:8089",
+#     "182.139.111.131:9000",
+#     "121.13.252.62:41564",
+#     "218.1.200.97:57114",
+#     "111.225.153.6:8089",
+#     "171.92.20.187:9000",
+#     "183.147.209.205:9000",
+#     "182.139.110.128:9000",
+#     "111.225.153.216:8089",
+#     "111.225.153.138:8089",
+#     "61.171.107.221:9002",
+#     "171.92.21.83:9000",
+#     "171.92.21.39:9000",
+#     "115.220.6.108:8089",
+#     "115.211.47.95:9000",
+#     "183.147.208.78:9000",
+#     "111.225.152.82:8089",
+#     "111.225.153.66:8089",
+#     "182.139.110.74:9000",
+#     "122.243.14.207:9000",
+#     "106.227.36.124:9002",
+#     "111.225.152.105:8089",
+#     "111.225.152.53:8089",
+#     "171.92.21.75:9000",
+#     "113.121.36.199:9999",
+#     "114.116.2.116:8001",
+#     "117.93.180.175:9000",
+#     "115.211.42.15:9000",
+#     "202.109.157.66:9000",
+#     "111.225.152.88:8089",
+#     "182.34.17.104:9999"]
+# ip_port = random.choice(proxy_arr)  # 随机选择一个代理
+# chrome_options.add_argument(f'--proxy-server=http://{ip_port}')  # 添加代理
+
 
 driver = webdriver.Chrome(options=chrome_options)  # 将chromedriver放到Python安装目录Scripts文件夹下
 
@@ -66,7 +105,7 @@ def check_pic_res():
     timestamp = re.findall(r'''timestamp=(.*?)"''',str(target_str))[0]
     req_headers = {
                     'USER_AGENT':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
-                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                    'Accept': 'text/html,application/xhtml+xml,applicatio6n/xml;q=0.9,*/*;q=0.8',
                     'Accept-Language': 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7',
                     'Connection':'keep-alive',
                     'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
@@ -83,10 +122,42 @@ def check_pic_res():
     print(pic_res)
     return pic_res
 
+def refresh_pic():
+    pp_html = driver.page_source
+    pic_path = '''/html/body/div[2]/form/div/div/div[2]/div[2]/img'''
+    target_pic_btn = driver.find_element(by=By.XPATH, value=pic_path)
+    target_pic_btn.click()
+    print('pit tick')
+
 
 driver.get('https://poll.cnfic.com.cn/vote2022/index.html')  # 此处不要再放登录的网址，可以用未登录的首页
 
 driver.refresh()
+
+
+
+
+pic_res = check_pic_res()
+pic_res.replace('~','')
+refresh_pic()
+
+if ('=?' in pic_res) or ('=7' in pic_res) :
+    do_next = 0
+    target_str = pic_res[:-2]
+    target_str = target_str.replace('=','')
+    ret_res = str(eval(target_str))
+    next_pic = 0
+
+elif pic_res.__len__() ==4 :
+    ret_res = pic_res
+    user_input = driver.find_element(by=By.XPATH, value='/html/body/div[2]/form/div/div/div[2]/div[1]/input')
+    user_input.send_keys(ret_res)
+    next_pic = 0
+
+elif pic_res.__len__() == 0:
+    refresh_pic()
+    pass
+
 
 target_path_16 = '''/html/body/div[2]/form/div/div/div[1]/div[16]/div[2]/div/input'''
 target_a_btn = driver.find_element(by=By.XPATH, value=target_path_16)
@@ -99,23 +170,6 @@ target_b_btn = driver.find_element(by=By.XPATH, value=target_path_14)
 time.sleep(1)
 target_b_btn.click()
 print('14 is choosed')
-
-pp_html = driver.page_source
-
-pic_path = '''/html/body/div[2]/form/div/div/div[2]/div[2]/img'''
-target_pic_btn = driver.find_element(by=By.XPATH, value=pic_path)
-target_pic_btn.click()
-print('pit tick')
-
-pic_res = check_pic_res()
-
-if '=?' in pic_res:
-    do_next = 0
-    target_str = pic_res[:-2]
-    target_str = target_str.replace('=','')
-    ret_res = str(eval(target_str))
-    user_input = driver.find_element(by=By.XPATH, value='/html/body/div[2]/form/div/div/div[2]/div[1]/input')
-    user_input.send_keys(ret_res)
 
 entry = '''/html/body/div[2]/form/div/div/div[2]/div[4]/button'''
 target_3_btn = driver.find_element(by=By.XPATH, value=entry)
