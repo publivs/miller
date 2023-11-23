91bc87bc899c9c6c806610a61f45679ea43e7f09
miller <517051861@qq.com>
2023-11-23 23:14:42 +0800

    update_提交新的修改_itchat

--- chanjiyue/itchat/test_run_1.py
+++ chanjiyue/itchat/test_run_1.py
@@ -2,11 +2,22 @@ import itchat
 import pandas as pd
 # 登录
 from itchat.content import *
-
+import time
+def generate_send_string():
+    return_text = f''' '''
 
 @itchat.msg_register([TEXT,NOTE])
 def text_reply(msg):
-    print(msg)
+    response = msg
+    # 车牌标记
+    target_string = '我通过'
+    if target_string in msg.Text:
+        itchat.send_msg(u"[%s]收到好友@%s 的信息：%s\n" % (time.strftime("%Y-%m-%d %H:%M:%S",
+                                                            time.localtime(msg['CreateTime'])),
+                                                            msg['User']['NickName'],
+                                                            msg['Text']),
+                                                            'filehelper')
+        print(msg)
 
 
 @itchat.msg_register([TEXT,NOTE],isGroupChat=True)
