ac33ee5025bdf8c92e50b51be74faa9aaf1d9567
chenqian <517051861@qq.com>
2024-07-02 17:25:32 +0800

    update_test_clickhouse_over

--- chenqian_tools/clickHouse/clickhouse_test.py
+++ chenqian_tools/clickHouse/clickhouse_test.py
@@ -17,23 +17,31 @@ import pandas as pd
 #         print("Test failed:", str(e))
 
 import clickhouse_driver 
+import time
 def test_clickhouse_connection_driverconn():
     try:
         con_dict = dict(host='127.0.0.1', port=9000, user='default', password='123456',database = 'factor_exposure_values')
         conn = clickhouse_driver.connect(**con_dict)
-        query = 'SELECT 1'
+        query = 'SELECT * from rgc_v0_momentum_1d'
+        t0 = time.time()
         df = pd.read_sql_query(query, conn)
-
+        t1 = time.time()
+        print('数据读取',t1 - t0)
+        conn.close()
+        print('df')
     except Exception as e:
         print("Test failed:", str(e))
 
-def sqlalchemy_test():
+
+
+
+def sqlalchemy_test(df):
+    from clickhouse_driver import Client
     from clickhouse_sqlalchemy import make_session
     from sqlalchemy import create_engine
     import pandas as pd
 
-    conf = dict(server_host='127.0.0.1', port=8123, user='default', password='123456',db = 'default')
-
+    conf = dict(server_host='127.0.0.1', port=8123, user='default', password='123456',db = 'factor_exposure_values')
     connection = 'clickhouse://{user}:{password}@{server_host}:{port}/{db}'.format(**conf)
     engine = create_engine(connection, pool_size=100, pool_recycle=3600, pool_timeout=20)
 
@@ -47,12 +55,21 @@ def sqlalchemy_test():
     # finally:
     #     cursor.close()
     #     session.close()
+
     # 新增
-    df.to_sql(name='rgc_v0_momentum_1d', con=engine, index=False, if_exists='append')
+    import time
+    t0 =time.time()
+    df.to_sql(name='rgc_v0_momentum_1d', con=engine, index=False, if_exists='append',)
+    t1 =time.time()
+    print(t1- t0,' 数据存储结束...')
 
 if __name__ == "__main__":
 
-    test_clickhouse_connection_driverconn()
+    # test_clickhouse_connection_driverconn()
 
     file_p = rf'''C:\Users\1\Desktop\miller\chenqian_tools\clickHouse\factors_parquet\rgc_v0_momentum_1d.parquet'''
     df = pd.read_parquet(file_p)
+    sqlalchemy_test(df)
+    
+    test_clickhouse_connection_driverconn()
+
